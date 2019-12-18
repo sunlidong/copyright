67736203d5f9b8f759279acdcd213205d71dbe6e
sunlidong <654460176@qq.com>
2019-12-18 16:33:49 +0800

    2

--- clear.sh
+++ clear.sh
@@ -2,3 +2,6 @@ docker rm $(docker ps -aq)
 docker rmi $(docker images dev-* -q)
 docker rmi $(docker images net-* -q)
 docker rmi $(docker images dev-peer* -q)
+sudo docker rm -f $(sudo docker ps -aq)
+sudo docker network prune
+sudo docker volume prune
