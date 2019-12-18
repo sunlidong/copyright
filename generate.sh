74d91ed0d215f69e4ba3dc78b1afcfd8f521872d
sld <654460176@qq.com>
2019-12-18 13:50:30 +0800

    init

--- generate.sh
+++ generate.sh
@@ -5,33 +5,33 @@
 ## SPDX-License-Identifier: Apache-2.0
 ##
 
-CHANNEL_NAME="assetpublish"
+CHANNEL_NAME="channelcopyright"
 
 sleep 1
 
 # generate genesis block for orderer
-configtxgen -profile SampleMultiNodeEtcdRaft   -outputBlock  ./config/genesis.block
+configtxgen -profile BqchainMultiNodeEtcdRaft   -outputBlock  ./config/genesis.block
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate orderer genesis block..."
   exit 1
 fi
 
 # generate channel configuration transaction
-configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
+configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/$CHANNEL_NAME.tx -channelID $CHANNEL_NAME
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate channel configuration transaction..."
   exit 1
 fi
 
 # generate anchor peer transaction
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for ProductOrg..."
   exit 1
 fi
 
 # generate anchor peer transaction two
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./config/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for FactoringOrg..."
   exit 1
