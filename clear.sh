991b63768e82306351bfb090fb396e9ed7356327
sld <654460176@qq.com>
2019-12-18 17:42:02 +0800

    init

--- clear.sh
+++ clear.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+### 删除镜像以及容器
 docker rm $(docker ps -aq)
 docker rmi $(docker images dev-* -q)
 docker rmi $(docker images net-* -q)
@@ -8,6 +10,6 @@ sudo docker rm -f $(sudo docker ps -aq)
 sudo docker network prune
 sudo docker volume prune
 
-###
+### 删除 缓存目录
 rm -rf ./channel-artifacts/*
 rm -rf ./crypto-config/*
